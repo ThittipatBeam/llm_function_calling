a1ed24c40acf88025f18a9962e25562f93253eb5
siam <team@siam.ai>
2024-12-09 04:58:26 +0000

    modified:   main.py 	modified:   requirement.txt

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ import os
 from dotenv import load_dotenv
 import requests
 from typing import List
+import json
+from datetime import datetime
 
+import streamlit as st
+
+import torch
 from langchain_huggingface.llms import HuggingFacePipeline
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_huggingface import HuggingFacePipeline, ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.messages import SystemMessage, AIMessage, HumanMessage, ToolMessage
 
 from pydantic import BaseModel, Field
 
@@ -48,7 +54,7 @@ def get_current_weather(city_name: str):
         print(f"An error occurred: {e}")
 
 
-# @st.cache_resource
+@st.cache_resource
 def load_local_model():
 
     # return HuggingFaceEndpoint(
@@ -63,17 +69,21 @@ def load_local_model():
     #         task="text-generation",
     #         pipeline_kwargs={
     #             "max_new_tokens": 1024,
+    #             "cache_dir": "/mnt/c/Users/beam_/Desktop/llm_function-calling/model"
     #             # "top_k": 50,
     #             # "temperature": 0.4
     #         },
     #     )
 
+    os.getenv("CUDA_VISIBLE_DEVICES", '')
+
     tokenizer = AutoTokenizer.from_pretrained(llm_model)
-    model = AutoModelForCausalLM.from_pretrained(llm_model)
+    model = AutoModelForCausalLM.from_pretrained(llm_model, device_map="auto")
     pipe = pipeline("text-generation",
                     model=model, 
                     tokenizer=tokenizer, 
-                    max_new_tokens=1024)
+                    max_new_tokens=1024,
+                    )
     hf = HuggingFacePipeline(pipeline=pipe)
     
     return hf
@@ -131,20 +141,77 @@ def prompt_ai(messages, nested_calls=0, invoked_tools=[]):
 
     try:
         ai_response = chatbot.invoke(messages)
-        return ai_response
     except:
         return prompt_ai(messages, nested_calls + 1)
+    print(ai_response)
+
+    has_tool_calls = len(ai_response["tool_calls"]) > 0
+    if has_tool_calls:
+        # Next, for each tool the AI wanted to call, call it and add the tool result to the list of messages
+        for tool_call in ai_response["tool_calls"]:
+            if str(tool_call) not in invoked_tools:
+                tool_name = tool_call["name"].lower()
+                selected_tool = available_tools[tool_name]
+                tool_output = selected_tool(**tool_call["args"])
+
+                messages.append(AIMessage(content=f"Thought: - I called {tool_name} with args {tool_call['args']} and got back: {tool_output}."))  
+                invoked_tools.append(str(tool_call))  
+            else:
+                return ai_response          
+
+        # Prompt the AI again now that the result of calling the tool(s) has been added to the chat history
+        return prompt_ai(messages, nested_calls + 1, invoked_tools)
+
+    return ai_response
+
+from langchain_core.prompts import PromptTemplate
+
+
+def prompt_ai_test(messages):
+
+    # template = tool_text
+    # prompt = PromptTemplate.from_template(template)
+    chatbot = ChatHuggingFace(llm=llm)
+
+    chain = chatbot
+    answer = chain.invoke(messages)
+
+    return print(answer.content)
+
+
+def main():
+    st.title("Weather Chatbot")
+
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = [
+            SystemMessage(content=f"You are a personal assistant who helps get the current weather base on the user input. The current date is: {datetime.now().date()}.\n{tool_text}")
+        ]
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        message_json = json.loads(message)
+        message_type = message_json["type"]
+        message_content = message_json["content"]
+        if message_type in ["human", "ai", "system"] and not message_content.startswith("Thought:"):
+            with st.chat_message(message_type):
+                st.markdown(message_content)        
+
+    # React to user input
+    if prompt := st.chat_input("What would you like to do today?"):
+        # Display user message in chat message container
+        st.chat_message("user").markdown(prompt)
+        # Add user message to chat history
+        st.session_state.messages.append(HumanMessage(content=prompt))
+
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            ai_response = prompt_ai(st.session_state.messages)
+            st.markdown(ai_response['content'])
+        
+        st.session_state.messages.append(AIMessage(content=ai_response['content']))
     
 
-
-
-
 if __name__ == "__main__":
-
-    a = prompt_ai(messages="สวัสดีครับ")
-    print(a)
-    print("beam")
-    print("kuy")
-
-
-
+    # main()
+    prompt_ai_test("Hello")
